b38161b0cce6f07f7636f7a0367e7bd06fe41063
Oleksandra <karska.dev@gmail.com>
2020-05-24 15:48:26 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var character = document.getElementById('character');
+var block = document.getElementById('block');
+function jump() {
+    if (character.classList != 'animate')
+        character.classList.add('animate');
+    setTimeout(function(){
+        character.classList.remove('animate');
+    }, 750)
+}
+
+var checkDead = setInterval(
+    function () {
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+        var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+        if (blockLeft<165 && 
+            blockLeft>145 &&
+            characterTop>=130){
+            block.style.animation = "none";
+            block.style.display = "none";
+            console.log(`Left block is: ${blockLeft} and characterTop is: ${characterTop}`);
+            alert('you lose!');
+        }
+    }, 10);
